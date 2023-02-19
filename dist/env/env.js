d93fbdee8fe36efced3ffb8047987558ba6d437f
Panem <titoluccao@gmail.com>
2023-02-19 20:56:53 -0300

    fix: dist on gitignore

--- dist/env/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = void 0;
-const schema_1 = require("./schema");
-exports.env = schema_1.environmentSchema.parse({
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-});
