52c55d47dec60408389ba51c021320829a825ec3
Panem <titoluccao@gmail.com>
2023-04-23 13:35:45 -0300

    style: add linting rules

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+}
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,72 @@
+const path = require("path");
+
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+  overrides: [
+    {
+      files: ["*.js", "*.cjs"],
+      rules: {
+        "no-undef": "off",
+        "@typescript-eslint/no-var-requires": "off",
+      },
     },
+    {
+      extends: [
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      ],
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: "@chimera/tsconfig/nextjs.json",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "@chimera/tsconfig/base.json",
+  },
+  plugins: ["@typescript-eslint", "prettier", "simple-import-sort"],
+  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "space-before-function-paren": "off",
+    "padded-blocks": "off",
+    "no-trailing-spaces": "warn",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
+    "eol-last": ["warn", "always"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "no-multiple-empty-lines": [
+      "warn",
+      {
+        max: 1,
+      },
+    ],
+    "prettier/prettier": [
+      "warn",
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ],
+    "max-len": [
+      "warn",
+      {
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
