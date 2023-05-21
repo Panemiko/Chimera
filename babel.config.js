a44f9f239b49380879bdb60433b222ba490b93c9
Panem <titoluccao@gmail.com>
2023-05-20 22:41:04 -0300

    feat: init with expo

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
+process.env.EXPO_ROUTER_APP_ROOT = "../../src/app";
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      ["module:react-native-dotenv"],
       require.resolve("expo-router/babel"),
+      "nativewind/babel",
     ],
   };
 };
