3e3418a56bbfceb425b06910d781e6aacb687b78
Panem <titoluccao@gmail.com>
2023-05-20 22:51:41 -0300

    feat: setup nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Entrada</Text>
+    <View className="flex-1 bg-neutral-900 justify-center items-center">
+      <Text className="text-neutral-50">Home</Text>
+      <StatusBar barStyle={"light-content"} />
     </View>
   );
 }
